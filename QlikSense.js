9ac7b5b95721437ca297a6a4b127e6d135cbcf6d
Flowconnect <40149624+Flowconnect@users.noreply.github.com>
2018-08-06 13:44:18 +0200

    Add files via upload

---
+++ QlikSense.js
@@ -0,0 +1,37 @@
+/*
+ * Basic responsive mashup template
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+require.config( {
+	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
+} );
+
+require( ["js/qlik"], function ( qlik ) {
+	qlik.setOnError( function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	var app = qlik.openApp('6aeb4aa7-fb64-48bd-be28-c939cc4f13e3', config);
+
+	//get objects -- inserted here --
+	app.getObject('CurrentSelections','CurrentSelections');
+	app.getObject('QV01','jLtqUHA');
+	//create cubes and lists -- inserted here --
+
+} );
